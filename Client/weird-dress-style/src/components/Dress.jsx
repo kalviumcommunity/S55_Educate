acc2f21ccc59734b71bd37fd52a568bd29fced91
Bhumika-Raut <bhumikaraut59620@gmail.com>
2024-02-29 10:34:06 +0530

    added deployed link in read.e.md

--- Client/weird-dress-style/src/components/Dress.jsx
+++ Client/weird-dress-style/src/components/Dress.jsx
@@ -2,27 +2,28 @@
 
 function Dress() {
   return (
-    <div>
-      <Navbar />
-      <div className="content">
-        <h1>Welcome to Our Website</h1>
-        <p>Find your choice here.</p>
-      </div>
-    </div>
+  <div>
+  <Navbar />
+  <div className="content">
+  <h1>Welcome to Our Website</h1>
+  <p>Find your choice here.</p>
+  </div>
+  </div>
   );
-}
+  }
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="container">
-        <div className="navbar-logo">
+    <div className="container">
+    <div className="navbar-logo">
         
-        </div>
-        <ul className="navbar-links">
-           </ul>
-        <SearchBar />
-      </div>
+    </div>
+    <ul className="navbar-links">
+    </ul>
+    <SearchBar />
+
+    </div>
     </nav>
   );
 }
@@ -30,8 +31,9 @@ function Navbar() {
 function SearchBar() {
   return (
     <div className="search-bar">
-      <input className='searchspace' type="text" placeholder="Search..." />
-      <button>Search</button>
+    <input className='searchspace' type="text" placeholder="Search..." />
+    <button>Search</button>
+    
     </div>
   );
 }
