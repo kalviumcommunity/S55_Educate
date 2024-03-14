946e7aa1223711b262102ea8a698361f7aa869de
Bhumika-Raut <bhumikaraut59620@gmail.com>
2024-03-14 15:55:36 +0530

    Created form

--- Server/schema.js
+++ Server/schema.js
@@ -1,15 +1,12 @@
 const mongoose = require('mongoose');
 
 const EntitySchema = new mongoose.Schema({
-    Dress: {
         Entity: String,
         Property1: String,
         Property2: String,
         Property3: String,
         Rating: Number,
         img: String,
-
-    }
 });
 
 const Entity = mongoose.model('dress-collections', EntitySchema); 
