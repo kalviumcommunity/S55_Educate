9609d929d5c4d48e0e69988645e1334177cf52db
Bhumika-Raut <bhumikaraut59620@gmail.com>
2024-03-16 09:38:38 +0530

    repairing

--- Server/schema.js
+++ Server/schema.js
@@ -1,31 +1,14 @@
 const mongoose = require('mongoose');
 
 const EntitySchema = new mongoose.Schema({
-    Entity: {
-        type: String,
-        required: true
-    },
-    Property1: {
-        type: String,
-        required: true
-    },
-    Property2: {
-        type: String,
-        required: true
-    },
-    Property3: {
-        type: String,
-        required: true
-    },
-    Rating: {
-        type: Number,
-        required: true,
-        min: 0,
-        max: 5
-    },
-    img: String,
+        Entity: String,
+        Property1: String,
+        Property2: String,
+        Property3: String,
+        Rating: Number,
+        img: String,
 });
 
-const Entity = mongoose.model('dress-collections', EntitySchema);
+const Entity = mongoose.model('dress-collections', EntitySchema); 
 
-module.exports = { Entity };
+module.exports = {Entity};
